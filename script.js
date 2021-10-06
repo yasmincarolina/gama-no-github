74f961cfa19f8771fe61fe537300c326cb3bd542
yasmincarolina <carolsousaguimaraes@gmail.com>
2021-10-06 20:57:26 -0300

    fazendo meu 2o commit

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
-nome = prompt("Qual é o nome do produto?")
-valor = parseInt(prompt("Qual é o valor do produto?"))
-desc1 = (valor - (valor * 0.05))
-desc2 = (valor - (valor * 0.1))
-desc3 = (valor - (valor * 0.15))
+nomeE1 = prompt("Digite o nome de Estudante 1:")
+notasE1 = prompt("Digite as 3 notas de Estudante 1:")
+notasE1 = notasE1.split(",")
 
-alert("O nome digitado foi: " + nome)
+mediaE1 = ( parseFloat(notasE1[0]) + parseFloat(notasE1[1]) + parseFloat(notasE1[2]) ) / 3
 
-if(valor < 100){
-    alert("O valor final do produto é R$" + valor)
+if(mediaE1 >= 5){
+    alert(nomeE1 + " obteve aprovação na média.")
 }
-if(valor >= 100 && valor <= 200){
-    alert("O valor final do produto é R$" + desc1)
+else if(mediaE1 < 5){
+    alert(nomeE1 + " não obteve aprovação na média.")
 }
-if(valor >= 201 && valor <= 250){
-    alert("O valor final do produto é R$" + desc2)
+
+nomeE2 = prompt("Digite o nome de Estudante 2:")
+notasE2 = prompt("Digite as 3 notas de Estudante 2:")
+notasE2 = notasE2.split(",")
+
+mediaE2 = ( parseFloat(notasE2[0]) + parseFloat(notasE2[1]) + parseFloat(notasE2[2]) ) / 3
+
+if(mediaE2 >= 5){
+    alert(nomeE2 + " obteve aprovação na média.")
 }
-if(valor >= 251){
-    alert("O valor final do produto é R$" + desc3)
+else if(mediaE2 < 5){
+    alert(nomeE2 + " não obteve aprovação na média.")
+}
+
+nomeE3 = prompt("Digite o nome de Estudante 3:")
+notasE3 = prompt("Digite as 3 notas de Estudante 3:")
+notasE3 = notasE3.split(",")
+
+mediaE3 = ( parseFloat(notasE3[0]) + parseFloat(notasE3[1]) + parseFloat(notasE3[2]) ) / 3
+
+if(mediaE3 >= 5){
+    alert(nomeE3 + " obteve aprovação na média.")
+}
+else if(mediaE3 < 5){
+    alert(nomeE3 + " não obteve aprovação na média.")
+}
+
+nomeE4 = prompt("Digite o nome de Estudante 4:")
+notasE4 = prompt("Digite as 3 notas de Estudante 4:")
+notasE4 = notasE4.split(",")
+
+mediaE4 = ( parseFloat(notasE4[0]) + parseFloat(notasE4[1]) + parseFloat(notasE4[2]) ) / 3
+
+if(mediaE4 >= 5){
+    alert(nomeE4 + " obteve aprovação na média.")
+}
+else if(mediaE4 < 5){
+    alert(nomeE4 + " não obteve aprovação na média.")
+}
+
+nomeE5 = prompt("Digite o nome de Estudante 5:")
+notasE5 = prompt("Digite as 3 notas de Estudante 5:")
+notasE5 = notasE5.split(",")
+
+mediaE5 = ( parseFloat(notasE5[0]) + parseFloat(notasE5[1]) + parseFloat(notasE5[2]) ) / 3
+
+if(mediaE5 >= 5){
+    alert(nomeE5 + " obteve aprovação na média.")
+}
+else if(mediaE5 < 5){
+    alert(nomeE5 + " não obteve aprovação na média.")
 }
